f6bf046c117207faeb116ecf80408a3b72614553
Raul Lucas <raullinjetmc@gmail.com>
2025-07-06 16:39:23 -0500

    added add and display entry features and fixed bugs

--- jobs/main.py
+++ jobs/main.py
@@ -1,20 +1,21 @@
 import random
 import sqlite3
 
+
 print("Welcome to JobSnap, the easy to-use job tracker for applications!")
 
 # connection to database -----------------------------------------------------
-connect = sqlite3.connect("jobs.db")
+connect = sqlite3.connect("data/jobs.db")
 cursor = connect.cursor()
 
 # table -----------------------------------------------------
 table = """
 CREATE TABLE IF NOT EXISTS APPINFO (
-    first_name TEXT
-    last_name TEXT
-    app_title TEXT
-    status BOOLEAN
-    company TEXT
+    first_name TEXT,
+    last_name TEXT,
+    app_title TEXT,
+    status BOOLEAN,
+    company TEXT,
     email TEXT
 )
 """
@@ -46,32 +47,46 @@ if (userinput == '1'):
     print("You are now adding a j*b")
     
     addfnameinput = input("Enter your first name: ")
-    
-    
     addlnameinput = input("Enter your last name: ")
-    
-    
+    applicationtitle = input("Enter application name: ")
     statusinput = input("Have you applied or not (yes/no): ")
+    if (statusinput.lower() == 'yes'):
+        status_value = 1
+    else:
+        status_value = 2
     companyinput = input("Enter company applied to: ")
     emailinput = input("Enter your email: ")
     
     # INSERT VALUES INTO DATABASE
-    queryinsert = "INSERT INTO APPINFO (first_name, last_name, status, company, email) VALUES (?,?,?,?,?)"
+    queryinsert = "INSERT INTO APPINFO (first_name, last_name, app_title, status, company, email) VALUES (?,?,?,?,?,?)"
+    cursor.execute(queryinsert, (addfnameinput, addlnameinput, applicationtitle, status_value, companyinput, emailinput))
     connect.commit()
     
     print("Job entry success!")
-    
+
+
+
 elif (userinput == '2'):
+    
     print("You are now viewing a j*b")
+    cursor.execute("SELECT * FROM APPINFO")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
 
 elif (userinput == '3'):
     print("You are now editing a j*b")
-
+    
+    
+    queryedit = "UPDATE APPINFO (first_name, last_name, app_title, status, company, email) VALUES (?,?,?,?,?,?)"
+    
+"""
 elif (userinput == '4'):
     print("You are now deleting a j*b")
     
 else:
     print("Exiting")
     exit()
-    
 
+"""
